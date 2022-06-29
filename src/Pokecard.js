da6896635a4d1e36a934b0944bce4b9071bf1cd0
KavaughnR <90059968+KavaughnR@users.noreply.github.com>
2022-06-29 18:50:27 -0400

    Pokemon Commit

---
+++ src/Pokecard.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import './Pokecard.css'
+// const POKE_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+
+const POKE_API = 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/'
+
+let padTwoThree = (number) => ( number <= 999 ? `00${number}`.slice(-3): number)
+
+
+class Pokecard extends Component {
+    render() {
+        let imgSrc = `${POKE_API}${padTwoThree(this.props.id)}.png`;
+        return (
+            <div className='Pokecard'>   
+                <h1 className='Pokecard-title'>{this.props.name}</h1>
+                <img src={imgSrc} alt={this.props.name} />
+                <div className='Pokemon-data'> Type: {this.props.type} </div>
+                <div className='Pokemon-data'> Exp: {this.props.exp} </div>
+            </div>
+
+
+        ) 
+    }
+}
+
+export default Pokecard;
